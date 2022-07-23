d81bfc9e9d3aa5bd2d643870ef0d859462941a88
Santiago Fourcade <saintwish.01@gmail.com>
2022-07-23 14:16:56 -0300

    Agregados

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,20 @@
+import "./Navbar.css"
+
+const Navbar = () => {
+    return (
+        <nav className="Navbar">
+            <div>
+                <h1>
+                    E-Commers
+                </h1>
+                <div>
+                    <button>Celulares</button>
+                    <button>Tablets</button>
+                    <button>Notebooks</button>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
